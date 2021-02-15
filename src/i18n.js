2dd4e25be46a615e3e637493c72ba3271eba003c
ahmedmagdydev <ahmed.magdy.dev@gmail.com>
2021-02-15 15:14:40 +0300

    fix translation path on sharepoint

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,19 @@
-import i18n from "i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import { siteBaseUrl } from './constants/api';
 
+i18n.use(Backend).init({ loadPath: `${siteBaseUrl}locales/{{lng}}/{{ns}}.json` });
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
-    debug: false,
+    fallbackLng: 'en',
+    debug: true,
     detection: {
-      order: ["queryString", "cookie"],
-      cache: ["cookie"],
+      order: ['queryString', 'cookie'],
+      cache: ['cookie'],
     },
     interpolation: {
       escapeValue: false,
